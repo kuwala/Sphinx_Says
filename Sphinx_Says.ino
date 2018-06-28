7958faa47f9e1ea1ad623fe5e69e3649a9ea698c
Daniel Smolentsev <kuwala@gmail.com>
2018-06-28 14:32:12 -0700

    cleaned out confusing parts and removed instrument mode

--- Sphinx_Says.ino
+++ Sphinx_Says.ino
@@ -1,31 +1,8 @@
 #include <Arduino.h>
 #include "FastLED.h"
 #include "Bounce.h"
-// The Plan
-// a bunch of events get stored in arrays
-/*
-[1][2][1][2][3][3]
-you follow those events with inputs
-[1][2][1][3][2]
-untill you loose
-every couple (10) events an animation plays
-event manager
-plays events
-generate new events and assign them to arrays
-increase arrayLengthCounter
-read inputs
-*/
 /* Variables */
 
-/* Story
-
-SphinxCat Says
-Your pet cat has awaked its true egyption soul sucking potential
-Pray to it and feed it souls or else the world will be destroyed
-
-
-
-*/
 Bounce button1 = Bounce(1, 5);
 Bounce button2 = Bounce(2, 5);
 Bounce button3 = Bounce(3, 5);
@@ -274,113 +251,7 @@ int buttonToScaleInterval(int button) {
   }
   return interval;
 }
-void dannyLamp() {
-  for (size_t i = 0; i < 255; i++) {
-    allButtonsToCat(CHSV(i,255,brightness));
-  }
-}
 void loop() {
-  //dannyLamp();
-  if (Serial.available() > 0) {
-    while(1) {
-      // Enter keyboard mode
-      button1.update();
-      button2.update();
-      button3.update();
-      button4.update();
-      button5.update();
-      button6.update();
-
-      //Presses
-      if(button1.fallingEdge()) {
-        usbMIDI.sendNoteOn(root, 127, 1);
-        leds[0][root % NUM_LEDS_PER_STRIP] = CHSV(224,255,brightness);
-        FastLED.show();
-
-      }
-      if(button1.risingEdge()) {
-        usbMIDI.sendNoteOff(root,127,1);
-        leds[0][root % NUM_LEDS_PER_STRIP] = CHSV(0,0,0);
-        FastLED.show();
-      }
-      if(button2.fallingEdge()) {
-        usbMIDI.sendNoteOn(root + 2, 127, 1);
-        leds[1][root % NUM_LEDS_PER_STRIP] = CHSV(244,255,brightness);
-        FastLED.show();
-      }
-      if(button2.risingEdge()) {
-        usbMIDI.sendNoteOff(root + 2,127,1);
-        leds[1][root % NUM_LEDS_PER_STRIP] = CHSV(0,0,0);
-        FastLED.show();
-      }
-      if(button3.fallingEdge()) {
-        usbMIDI.sendNoteOn(root + 5, 127, 1);
-        leds[2][root % NUM_LEDS_PER_STRIP] = CHSV(0,255,brightness);
-        FastLED.show();
-      }
-      if(button3.risingEdge()) {
-        usbMIDI.sendNoteOff(root + 5,127,1);
-        leds[2][root % NUM_LEDS_PER_STRIP] = CHSV(0,0,0);
-        FastLED.show();
-      }
-      if(button4.fallingEdge()) {
-        usbMIDI.sendNoteOn(root + 7, 127, 1);
-        leds[3][root % NUM_LEDS_PER_STRIP] = CHSV(64,255,brightness);
-        FastLED.show();
-      }
-      if(button4.risingEdge()) {
-        usbMIDI.sendNoteOff(root + 7,127,1);
-        leds[3][root % NUM_LEDS_PER_STRIP] = CHSV(0,0,0);
-        FastLED.show();
-      }
-      if(button5.fallingEdge()) {
-        usbMIDI.sendNoteOn(root + 10, 127, 1);
-        leds[4][root % NUM_LEDS_PER_STRIP] = CHSV(96,255,brightness);
-        FastLED.show();
-      }
-      if(button5.risingEdge()) {
-        usbMIDI.sendNoteOff(root + 10,127,1);
-        leds[4][root % NUM_LEDS_PER_STRIP] = CHSV(0,0,0);
-        FastLED.show();
-      }
-      if(button6.fallingEdge()) {
-        root += 1;
-        usbMIDI.sendNoteOn(root + 12, 127, 1);
-        leds[5][root % NUM_LEDS_PER_STRIP] = CHSV(160,255,brightness);
-        FastLED.show();
-      }
-      if(button6.risingEdge()) {
-        usbMIDI.sendNoteOff(root + 12,127,1);
-        leds[5][root % NUM_LEDS_PER_STRIP] = CHSV(0,0,0);
-        FastLED.show();
-      }
-
-      int interval = 0;
-      buttonPressed = -1;
-
-      // if(but1 == LOW ) {
-      //   buttonPressed = 0;
-      // } else if (but2 == LOW) {
-      //   buttonPressed = 1;
-      // } else if (but3 == LOW) {
-      //   buttonPressed = 2;
-      // } else if (but4 == LOW) {
-      //   buttonPressed = 3;
-      // } else if (but5 == LOW) {
-      //   buttonPressed = 4;
-      // } else if (but6 == LOW) {
-      //   buttonPressed = 5;
-      // }
-      //
-      //   if (buttonPressed > -1) {
-      //     interval = buttonToScaleInterval(buttonPressed);
-      //     usbMIDI.sendNoteOn(84+interval, 127, 0);
-      //     delay(40);
-      //     usbMIDI.sendNoteOff(84+interval, 127, 0);
-      //     delay(40);
-      //   }
-    }
-  }
   // play example pattern
   randomizePattern(currentPatternLength);
   for (size_t i = 0; i < currentPatternLength; i++) {
